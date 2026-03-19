962cbce7daa4083fdbca6f3ae5bcee161fb3b097
Taren Sanders <taren.sanders@acu.edu.au>
2026-03-19 11:04:48 +1100

    Harmonise BPIPD-21 (#11)
    * WIP: start harmonisation
    
    * Update dataschema
    
    * Complete harmonisation for BPIPD-21
    
    * Update BPIPD-21 yaml
    
    * Fix status check
    
    * Use correct status code for yaml
    
    * Fix harmonisation error

---
+++ R/tidy/tidy_BPIPD_21.R
@@ -0,0 +1,16 @@
+#' Template tidier for pre-harmonisation dataset shaping
+#'
+#' Use this step for study-specific table assembly before harmonisation, for
+#' example joining multiple raw files, binding waves, filtering records, or
+#' reshaping raw tables into one canonical analysis tibble.
+#'
+#' Input:
+#' - `raw_dataset`: output of `read_dataset_from_spec()`
+#' - `spec`: parsed dataset YAML
+#'
+#' Output:
+#' - one tibble to be used as the harmonisation input
+tidy_BPIPD_21 <- function(raw_dataset, spec) {
+  # No modifications needed
+  tibble::as_tibble(raw_dataset$data[[1]])
+}
