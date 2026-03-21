8638e95099ad209f6925a2dcad514f3ed72d056d
Taren Sanders <taren.sanders@acu.edu.au>
2026-03-21 19:52:59 +0000

    Process BPIPD-1639

---
+++ R/tidy/tidy_BPIPD_1639.R
@@ -0,0 +1,18 @@
+#' Template tidier for pre-harmonisation dataset shaping
+#'
+#' Use this step for study-specific table assembly before harmonisation, for
+#' example joining multiple raw files, binding waves, filtering records, or
+#' reshaping raw tables into one canonical analysis tibble.
+#'
+#' Input:
+#' - `raw_dataset`: output of `read_dataset_from_spec()`
+#' - `spec`: parsed dataset YAML
+#'
+#' Output:
+#' - one tibble to be used as the harmonisation input
+tidy_BPIPD_1639 <- function(raw_dataset, spec) {
+  df <- raw_dataset$data$demographics %>%
+    dplyr::full_join(raw_dataset$data$questionnaire, by = "personid") %>%
+    dplyr::full_join(raw_dataset$data$self_control, by = "personid")
+  df
+}
